48930284991c13cab341953e6e0007e8960d5168
ajsin <ajsin@host.docker.internal>
2024-09-26 17:41:30 +0530

    my first comit

---
+++ src/ExamplePro1/MyClassExp1.java
@@ -0,0 +1,14 @@
+package ExamplePro1;
+
+import java.util.Date;
+
+public class MyClassExp1 {
+
+	public static void main(String[] args) {
+		System.out.println("This si java program");
+		System.out.println("Output of java progarame");
+		System.out.println("hi I am comit");
+		System.out.println(new Date());
+	}
+
+}
