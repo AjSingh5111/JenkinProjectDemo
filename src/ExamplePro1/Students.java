23da2cdb0103f727853a810f95c89289d436b1c4
ajsin <ajsin@host.docker.internal>
2024-10-07 14:40:34 +0530

    it is my 6th commit

--- src/ExamplePro1/Students.java
+++ src/ExamplePro1/Students.java
@@ -5,7 +5,7 @@ public class Students {
 
 	public static void main(String[] args) {
 		
-		HelloAj ht=new HelloAj("Ajay", 101);
+		HelloAj ht=new HelloAj("Ajay Singh", 101);
 		System.out.println(ht.getRoll()+" "+ht.getName());
 	}
 
