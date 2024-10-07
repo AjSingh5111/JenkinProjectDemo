f2e22299f662646cecc8bc27bc11004a1e27537a
ajsin <ajsin@host.docker.internal>
2024-10-07 11:31:19 +0530

    this my 7Oct commit

--- src/ExamplePro1/MyClassExp1.java
+++ src/ExamplePro1/MyClassExp1.java
@@ -6,8 +6,9 @@ public class MyClassExp1 {
 
 	public static void main(String[] args) {
 		System.out.println("This si java program");
-		System.out.println("Output of java progarame");
 		System.out.println("hi I am comit");
+		System.out.println("hi this is Ajay Singh commit to complete the project..");
+		
 		System.out.println(new Date());
 	}
 
